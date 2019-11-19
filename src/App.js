66b295de52089dbd2ac3d5b8979b1ec9d5aa426c
Wendy Bartos <wendyebartos@gmail.com>
2019-11-19 14:02:02 -0800

    added all routes

--- src/App.js
+++ src/App.js
@@ -14,46 +14,89 @@ class App extends Component {
 
   render() {
     const { folders, notes } = this.state;
-    return ( <main className="App">
-      <header>
-        <Link to='/'>
-          <Header/>
-        </Link>
-      </header>
-      <section className="Content">
-        <Switch>
-          <Route exact={true} path="/" render={routeProps => {
-              return ( <div className="Content">
-                <Sidebar folderId={folders.find( folder => folder.id === routeProps.match.params.folderId )} folders={folders} {...routeProps}/>
-                <NoteList notes={this.props.store.notes}/>
-              </div> );
-            }}/>
+    return (
+      <main className="App">
+        <header>
+          <Link to="/">
+            <Header />
+          </Link>
+        </header>
+        <section className="Content">
+          <Switch>
+            <Route
+              exact={true}
+              path="/"
+              render={routeProps => {
+                return (
+                  <div className="Content">
+                    <Sidebar
+                      folderId={folders.find(
+                        folder => folder.id === routeProps.match.params.folderId
+                      )}
+                      folders={folders}
+                      {...routeProps}
+                    />
+                    <NoteList notes={this.props.store.notes} />
+                  </div>
+                );
+              }}
+            />
 
-          <Route path="/folder/:folderId" folders={folders} render={routeProps => {
-              console.log( routeProps.match );
-              const filteredNotes = this.props.store.notes.filter( n => n.folderId === routeProps.match.params.folderId )
+            <Route
+              path="/folder/:folderId"
+              folders={folders}
+              render={routeProps => {
+                console.log(routeProps.match);
+                const filteredNotes = this.props.store.notes.filter(
+                  n => n.folderId === routeProps.match.params.folderId
+                );
 
-              return ( <div className="Content">
-                <Sidebar folderId={folders.find( folder => folder.id === routeProps.match.params.folderId )} folders={folders} {...routeProps}/>
-                <NoteList notes={filteredNotes}/>
-              </div> );
-            }}/>
+                return (
+                  <div className="Content">
+                    <Sidebar
+                      folderId={folders.find(
+                        folder => folder.id === routeProps.match.params.folderId
+                      )}
+                      folders={folders}
+                      {...routeProps}
+                    />
+                    <NoteList notes={filteredNotes} />
+                  </div>
+                );
+              }}
+            />
 
-          <Route path="/note/:noteId" folders={folders} render={routeProps => {
-              console.log( routeProps.match );
-              const selectedNote = this.props.store.notes.find( n => n.id === routeProps.match.params.noteId )
+            <Route
+              path="/note/:noteId"
+              folders={folders}
+              render={({ match, history }) => {
+                const selectedNote = this.props.store.notes.find(
+                  n => n.id === match.params.noteId
+                );
 
-              console.log( selectedNote )
+                console.log(folders);
+                const folderName = folders.find(
+                  folder => selectedNote.folderId === folder.id
+                );
 
-              return ( <div className="Content">
-                <Sidebar folders={folders} {...routeProps}/>
-                <NoteList notes={selectedNote}/>
-              </div> );
-            }}/>
-        </Switch>
-
-      </section>
-    </main> );
+                return (
+                  <div className="Content">
+                    <h3>{folderName.name}</h3>
+                    <button
+                      className="Go-back"
+                      onClick={() => history.goBack()}
+                    >
+                      Go back
+                    </button>
+                    <NoteList notes={selectedNote} />
+                  </div>
+                );
+              }}
+            />
+          </Switch>
+        </section>
+      </main>
+    );
   }
 }
 // <Route render={routeProps => ( <Sidebar folders={folders}/> )}/>
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,24 +2,45 @@ import React from "react";
 import Note from "./Note";
 import "./Note.css";
 
-export default function NoteList( props ) {
-  // const filteredList = props.notes.filter(note.id===note.folderId)
+export default function NoteList(props) {
   let noteList;
 
-  if ( Array.isArray( props.notes ) ) {
-    noteList = props.notes.map( note => {
-      console.log( props, note.id, note.folderId );
-      return ( <Note showContent={false} key={note.id} id={note.id} name={note.name} modified={note.modified} folderId={note.folderId} content={note.content}/> );
-    } );
-  } else 
-    noteList = <Note showContent={true} key={props.notes.id} id={props.notes.id} name={props.notes.name} modified={props.notes.modified} folderId={props.notes.folderId} content={props.notes.content}/>
+  if (Array.isArray(props.notes)) {
+    noteList = props.notes.map(note => {
+      console.log(props, note.id, note.folderId);
+      return (
+        <Note
+          showContent={false}
+          key={note.id}
+          id={note.id}
+          name={note.name}
+          modified={note.modified}
+          folderId={note.folderId}
+          content={note.content}
+        />
+      );
+    });
+  } else
+    noteList = (
+      <Note
+        showContent={true}
+        key={props.notes.id}
+        id={props.notes.id}
+        name={props.notes.name}
+        modified={props.notes.modified}
+        folderId={props.notes.folderId}
+        content={props.notes.content}
+      />
+    );
 
-  return Array.isArray( noteList )
-    ? ( <div>
+  return Array.isArray(noteList) ? (
+    <div>
       <ul className="Note-list">{noteList}</ul>
       <button className="Add-note">Add note</button>
-    </div> )
-    : ( <div>
+    </div>
+  ) : (
+    <div>
       <ul className="Note-list">{noteList}</ul>
-    </div> );
+    </div>
+  );
 }
